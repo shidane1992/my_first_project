d5c3cb444e9d34de97e0b91a6fcb01c97a0cca60
root <root@abdinasir.localdomain>
2025-06-01 20:56:26 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Greeting from "./components/Greeting";
+import Header from "./components/Header";
+function App() {
+  return (
+    <>
+      <Greeting name="Abdinasir " surname="shidane" />
+      <Header />
+      
+    </>
+  );
+}
+
+export default App;
